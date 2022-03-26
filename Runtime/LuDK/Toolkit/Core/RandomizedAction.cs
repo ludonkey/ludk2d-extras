ae073de3824c6f39c3fa19a0da3e107cdb3edafc
ludk <equesada@ludonkey.com>
2022-03-26 17:42:14 +0100

    [FEATURE] RandomizedAction to be able to random act an action

---
+++ Runtime/LuDK/Toolkit/Core/RandomizedAction.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LuDK.Toolkit.Core
+{
+    public class RandomizedAction : MonoBehaviour
+    {
+        public bool playOnStart = false;
+        public float delay = 0;
+        public List<UnityEvent> OnAct;
+        private float timeBeforeAct = -1;
+
+        private void Awake()
+        {
+            if (null == OnAct)
+                OnAct = new List<UnityEvent>();
+        }
+
+        private void Start()
+        {
+            if (playOnStart)
+            {
+                DelayedAct(delay);
+            }
+        }
+
+        public void Act()
+        {
+            DelayedAct(0);           
+        }
+
+        public void DelayedAct(float delay)
+        {
+            if (OnAct != null && OnAct.Count > 0)
+            {
+                if (delay > 0)
+                {
+                    timeBeforeAct = delay;
+                }
+                else
+                {
+                    UnityEvent oneRandomizedAction = OnAct[Random.Range(0, OnAct.Count)];
+                    oneRandomizedAction?.Invoke();
+                }
+            }
+        }
+
+        private void Update()
+        {
+            if (timeBeforeAct > 0)
+            {
+                timeBeforeAct -= Time.deltaTime;
+                if (timeBeforeAct <= 0)
+                {
+                    timeBeforeAct = -1;
+                    if (OnAct != null && OnAct.Count > 0)
+                    {
+                        UnityEvent oneRandomizedAction = OnAct[Random.Range(0, OnAct.Count)];
+                        oneRandomizedAction?.Invoke();
+                    }
+                }
+            }
+        }
+    }
+}
