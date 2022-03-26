6ae4c002546dea585bd6f75616dda637051be1b4
ludk <equesada@ludonkey.com>
2022-03-26 17:18:33 +0100

    [FEATURE] Console to be able to Debug.Log

---
+++ Runtime/LuDK/Toolkit/Core/Console.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LuDK.Toolkit.Core
+{
+    public class Console : MonoBehaviour
+    {
+        public void Log(string msg)
+        {
+            Debug.Log(msg);       
+        }
+
+        public void LogWarning(string msg)
+        {
+            Debug.LogWarning(msg);
+        }
+
+        public void LogError(string msg)
+        {
+            Debug.LogError(msg);
+        }
+    }
+}
