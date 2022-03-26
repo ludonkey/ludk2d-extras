88eb3f1d72e7dd5e90f4c23aeaff8c9da3cdb088
ludk <equesada@ludonkey.com>
2022-03-26 17:41:00 +0100

    [CHANGE] Improved the Spawner with a list to be able to random the spawned object

--- Runtime/LuDK/Toolkit/Core/Spawner.cs
+++ Runtime/LuDK/Toolkit/Core/Spawner.cs
@@ -1,17 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace LuDK.Toolkit.Core
 {
     public class Spawner : MonoBehaviour
     {
-        public GameObject originalGameObject;
+        public List<GameObject> originalGameObjects;
 
         public UnityEvent OnSpawn;
 
+        private void Awake()
+        {
+            if (null == originalGameObjects)
+            {
+                originalGameObjects = new List<GameObject>();
+            }
+        }
+
         public void Spawn()
         {
-            var newGO = Instantiate(originalGameObject);
+            if (null == originalGameObjects || originalGameObjects.Count == 0)
+                return;
+            var newGO = Instantiate(originalGameObjects[Random.Range(0, originalGameObjects.Count)]);
             newGO.transform.position = transform.position;
             newGO.transform.rotation = transform.rotation;
             newGO.transform.localScale = transform.localScale;
