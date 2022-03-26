43f046eb1be3076a7faeae19e7091eb0ac0e97c6
ludk <equesada@ludonkey.com>
2022-03-26 17:18:57 +0100

    [FEATURE] Console to spawn GameObject

---
+++ Runtime/LuDK/Toolkit/Core/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace LuDK.Toolkit.Core
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject originalGameObject;
+
+        public UnityEvent OnSpawn;
+
+        public void Spawn()
+        {
+            var newGO = Instantiate(originalGameObject);
+            newGO.transform.position = transform.position;
+            newGO.transform.rotation = transform.rotation;
+            newGO.transform.localScale = transform.localScale;
+            newGO.transform.parent = transform;
+            newGO.SetActive(true);
+            OnSpawn?.Invoke();
+        }
+    }
+}
